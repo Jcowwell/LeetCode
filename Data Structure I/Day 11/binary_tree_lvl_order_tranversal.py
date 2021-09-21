3bd6cedd828bfae9969c69f3d62266267e9117ac
Jcowell <Jcowwell@gmail.com>
2021-09-21 11:56:09 -0400

    inorder, postorder , and lvl order transversal

--- Data Structure I/Day 11/binary_tree_lvl_order_tranversal.py
+++ Data Structure I/Day 11/binary_tree_lvl_order_tranversal.py
@@ -8,6 +8,7 @@ Constraints
 """
 
 from typing import Optional, List
+from queue import Queue
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -17,10 +18,46 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    # FIXME: Fix Project
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        """
-        """
+    
+    def bfs(self, root: Optional[TreeNode]) -> List[List[int]] :
+        tree = []
+        queue = Queue()
+        depth: dict = {}
+        lvl: int = 0
+        # visited: set = set()
+        # visited.add(depth + root.val)
+        tree.append([root.val])
+        queue.put(root)
 
-        return [[]]
+        while not queue.empty():
+            # FIXME: use Queue size to help determine level
+            node: Optional[TreeNode] = queue.get()
+            nodes = []
+            level: int = len(queue)
+            if node.left:
+                nodes.append(node.left.val)
+                queue.put(node.left)
+                print(f'level: {lvl}')
+            if node.right:
+                nodes.append(node.right.val)
+                queue.put(node.right)
+                print(f'level: {lvl}')
+            if nodes:
+                lvl += 1
+                tree.append(nodes)
+            
+        return tree
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # no point of tranversing an empty tree
+        if not root:
+            return []
+        # if the root node is also a leaf, 
+        if not root.left and not root.right:
+            # return just the root node.  
+            return [[root.val]]
+
+        return self.bfs(root=root)
+        
+
                 
